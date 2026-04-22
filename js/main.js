4296b7e90751dae087df5f70388a3f6e4d586ca1
Hugo <pesehu_aliesma24@iesmedinazahara.es>
2026-04-22 14:07:14 +0200

    Terminar JS de boton de color de las zapas

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Cogemos la imagen grande
+let fotoZapatilla = document.getElementById("foto_zapa");
+
+// Cogemos los dos botones
+let botonBlanco = document.getElementById("btn_blanco");
+let botonNegro = document.getElementById("btn_negro");
+
+// Le decimos qué hacer al botón blanco
+botonBlanco.addEventListener("click", function() {
+    fotoZapatilla.src = "../img/zapas/nikep6000blancas.png";
+});
+
+// Le decimos qué hacer al botón negro
+botonNegro.addEventListener("click", function() {
+    fotoZapatilla.src = "../img/zapas/nikep6000negras.png";
+});
